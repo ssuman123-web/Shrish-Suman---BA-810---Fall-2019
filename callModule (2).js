ca64fedcdca55e4fd2aafdd57e14563838fa1d5d
Shrish Suman - BA 810 - Fall 2019 <56237174+ssuman123-web@users.noreply.github.com>
2019-10-07 18:32:55 -0500

    Delete callModule (2).js

--- callModule (2).js
+++
@@ -1,8 +0,0 @@
-const moduleVariable = require('./modules.js');
-
-// console.log(moduleVariable(4,5));
-
-let moduleObject = new moduleVariable();
-
-moduleObject.testModule();
-console.log(moduleObject);
