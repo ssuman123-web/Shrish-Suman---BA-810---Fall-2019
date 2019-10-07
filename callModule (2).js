54ebb663416116f7455c87394a20dacfb1c3fddf
Shrish Suman - BA 810 - Fall 2019 <56237174+ssuman123-web@users.noreply.github.com>
2019-10-06 20:20:35 -0500

    This is call module

---
+++ callModule (2).js
@@ -0,0 +1,8 @@
+const moduleVariable = require('./modules.js');
+
+// console.log(moduleVariable(4,5));
+
+let moduleObject = new moduleVariable();
+
+moduleObject.testModule();
+console.log(moduleObject);
